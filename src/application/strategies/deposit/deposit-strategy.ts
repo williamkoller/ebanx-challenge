7c0b0e8cf91ac2cc3c5a8323c3390ce9c1d9ab36
William Koller <williamkoller30@gmail.com>
2025-02-11 14:46:39 -0300

    feat: move files

---
+++ src/application/strategies/deposit/deposit-strategy.ts
@@ -0,0 +1,31 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { TransactionStrategy } from '../transaction-strategy.interface';
+import {
+  DepositDto,
+  TransactionMapper,
+} from '../../mappers/transaction-mapper';
+import { Account, Transaction } from '../../../domain/entities/account';
+
+export class DepositStrategy implements TransactionStrategy<DepositDto> {
+  execute(accounts: Record<string, Account>, transaction: Transaction) {
+    const { destination, amount } = transaction;
+
+    if (!destination) {
+      throw new HttpException(
+        'Destination account is required',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (!accounts[destination]) {
+      accounts[destination] = Account.create({
+        id: destination,
+        balance: 0,
+        transactions: [transaction],
+      });
+    }
+
+    accounts[destination].deposit(amount);
+    return TransactionMapper.mapDeposit(accounts[destination]);
+  }
+}
---
+++ src/application/strategies/transfer/transfer-startegy.ts
@@ -0,0 +1,46 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { TransactionStrategy } from '../transaction-strategy.interface';
+import {
+  TransactionMapper,
+  TransferDto,
+} from '../../mappers/transaction-mapper';
+import { Account, Transaction } from '../../../domain/entities/account';
+
+export class TransferStrategy implements TransactionStrategy<TransferDto> {
+  execute(
+    accounts: Record<string, Account>,
+    transaction: Transaction,
+  ): TransferDto | number {
+    const { destination, amount, origin } = transaction;
+
+    if (!destination) {
+      throw new HttpException(
+        'Destination account is required',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (!origin) {
+      throw new HttpException(
+        'Origin account is required',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    if (!accounts[destination]) {
+      accounts[destination] = Account.create({
+        id: destination,
+        balance: 0,
+        transactions: [transaction],
+      });
+    }
+    if (!accounts[origin]) {
+      return 0;
+    }
+    accounts[origin].transfer(amount, accounts[destination]);
+    return TransactionMapper.mapTransfer(
+      accounts[origin],
+      accounts[destination],
+    );
+  }
+}
