1c7e0bc71e10fc2c62a2bb5a981d5a4443caf12a
William Koller <williamkoller30@gmail.com>
2025-02-11 14:45:27 -0300

    feat: move files

--- src/shared/errors/domain-validation-exception.ts
+++
@@ -1,7 +0,0 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
-
-export class DomainValidationException extends HttpException {
-  constructor(message?: string) {
-    super(message || 'Domain validation exception', HttpStatus.BAD_REQUEST);
-  }
-}
